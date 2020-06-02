fe2da6b842922b1605ae178f52b23de7228cbce7
Luís Galete <luisgalete6264@gmail.com>
2020-06-01 22:19:12 -0300

    Add ex001 to ex005

---
+++ Lógica de programação/Python/ex004.py
@@ -0,0 +1,5 @@
+soma = 0.0
+for i in range(3):
+    soma += float(input('Digite os salarios: '))
+media = soma / 3
+print('A media dos salarios e ' + str(media))
---
+++ Lógica de programação/Python/ex005.py
@@ -0,0 +1,3 @@
+valor_m = float(input('Digite um valor em metros: '))
+valor_cm = valor_m * 100
+print('{} metros em cm equivale a {}'.format(valor_m, valor_cm))
