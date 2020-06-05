3c4a10bcfd523bb192a832fabb0225d72378e0b8
Luís Galete <luisgalete6264@gmail.com>
2020-06-04 21:22:12 -0300

    Add ex006.py & 007.py

---
+++ Lógica de programação/Python/ex007.py
@@ -0,0 +1,18 @@
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+# a)
+print('{} {} {}'.format(s1, s2, s3))
+
+# b)
+print(10 * (s1 + ' '))
+
+# c)
+print(s1 + ' ' + 2 * (s2 + ' ') + 3 * (s3 + ' '))
+
+# d)
+print(7 * (s1 + ' ' + s2 + ' '))
+
+# e)
+print(5 * (2 * s2 + s3 + ' '))
