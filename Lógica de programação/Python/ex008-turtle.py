4b12bdea55fb8fb157a4acf864152c55707d4e9a
Luís Galete <luisgalete6264@gmail.com>
2020-06-09 11:27:24 -0300

    Create ex008-turtle.py

---
+++ Lógica de programação/Python/ex008-turtle.py
@@ -0,0 +1,37 @@
+import turtle
+
+# turtle.resetscreen() limpa a tela
+
+tart = turtle.Turtle()
+tart.pensize(5)
+
+tart.circle(150)
+
+tart.penup()
+tart.forward(75)
+tart.left(90)
+tart.forward(200)
+tart.pendown()
+
+tart.begin_fill()
+tart.circle(25)
+tart.end_fill()
+
+tart.penup()
+tart.left(90)
+tart.forward(100)
+tart.right(90)
+tart.pendown()
+
+tart.begin_fill()
+tart.circle(25)
+tart.end_fill()
+
+tart.penup()
+tart.left(90)
+tart.forward(50)
+tart.left(90)
+tart.forward(75)
+tart.pendown()
+
+tart.circle(75, 175)
