688cbeea9fcaed968320b582441f8e9664928ed9
Luís Galete <61747990+yunger7@users.noreply.github.com>
2020-09-03 15:26:21 -0300

    Update ex012-lista-de-exercicios1.py

--- Lógica de programação/Python/ex012-lista-de-exercicios1.py
+++ Lógica de programação/Python/ex012-lista-de-exercicios1.py
@@ -1,153 +1,153 @@
-# # 2.12
-# # a)
-# print(1 + 2 + 3 + 4 + 5 + 6 + 7)
+# 2.12
+# a)
+print(1 + 2 + 3 + 4 + 5 + 6 + 7)
 
-# # b)
-# print((65 + 56 + 45) / 3)
+# b)
+print((65 + 56 + 45) / 3)
 
-# # c)
-# print (2 ** 20)
+# c)
+print (2 ** 20)
 
-# # d)
-# print(4356 // 61)
+# d)
+print(4356 // 61)
 
-# # e)
-# print(4365 % 61)
+# e)
+print(4365 % 61)
 
-# ==================================
+==================================
 
-# # 2.13
-# s1 = '-'
-# s2 = '+'
+# 2.13
+s1 = '-'
+s2 = '+'
 
-# # a)
-# print(s1 + s2)
+# a)
+print(s1 + s2)
 
-# # b)
-# print(s1 + s2)
+# b)
+print(s1 + s2)
 
-# # c)
-# print(s2 + s1 + s1)
+# c)
+print(s2 + s1 + s1)
 
-# # d)
-# print((s2 + 2 * s1)*2)
+# d)
+print((s2 + 2 * s1)*2)
 
-# # e)
-# print((s2 + 2*s1)*10 + s2)
+# e)
+print((s2 + 2*s1)*10 + s2)
 
-# # f)
-# print((s2 + s1 + 3*s2 + 2*s1)*5)
+# f)
+print((s2 + s1 + 3*s2 + 2*s1)*5)
 
-# ==================================
+==================================
 
-# # 2.14
+# 2.14
 
-# s = 'abcdefghijklmnopqrstuvwxyz'
+s = 'abcdefghijklmnopqrstuvwxyz'
 
-# print(s[0])
+print(s[0])
 
-# print(s[s.index('c')])
+print(s[s.index('c')])
 
-# print(s[-1])
+print(s[-1])
 
-# print(s[-2])
+print(s[-2])
 
-# print(s[s.index('q')])
+print(s[s.index('q')])
 
-# ==================================
+==================================
 
-# # 2.15
-# s = 'goodbye'
+# 2.15
+s = 'goodbye'
 
-# # a)
-# print(s[0] == 'g')
+# a)
+print(s[0] == 'g')
 
-# # b)
-# print(s[6] == 'g')
+# b)
+print(s[6] == 'g')
 
-# # c)
-# print(s[0] == 'g' and s[1] == 'a')
+# c)
+print(s[0] == 'g' and s[1] == 'a')
 
-# # d)
-# print(s[-2] == 'x')
+# d)
+print(s[-2] == 'x')
 
-# # e)
-# print(s[3] == 'd')
+# e)
+print(s[3] == 'd')
 
-# # f)
-# print(s[0] == s[-1])
+# f)
+print(s[0] == s[-1])
 
-# #g)
-# print(s[:4] == 'tion')
+#g)
+print(s[:4] == 'tion')
 
-# ==================================
+==================================
 
-# # 2.16
-# # a)
-# a = 6
-# b = 7
+# 2.16
+# a)
+a = 6
+b = 7
 
-# # b)
-# c = (a + b) / 2
+# b)
+c = (a + b) / 2
 
-# # c)
-# estoque = ['papel','grampos','lapis']
+# c)
+estoque = ['papel','grampos','lapis']
 
-# # d)
-# primeiro = 'John'
-# meio = 'Fitzgerald'
-# ultimo = 'Kennedy'
+# d)
+primeiro = 'John'
+meio = 'Fitzgerald'
+ultimo = 'Kennedy'
 
-# # e)
-# nomecompleto = '{} {} {}'.format(primeiro, meio, ultimo)
+# e)
+nomecompleto = '{} {} {}'.format(primeiro, meio, ultimo)
 
-# ==================================
+==================================
 
-# 2.17
-# Faltam instrucoes
+2.17
+Faltam instrucoes
 
-# ==================================
+==================================
 
-# # 2.18
-# # a)
-# flores = ['rosa','buganvilia','iuca','margarida','dalia','lirio dos vales']
+# 2.18
+# a)
+flores = ['rosa','buganvilia','iuca','margarida','dalia','lirio dos vales']
 
-# # b)
-# print('batata' in flores)
+# b)
+print('batata' in flores)
 
-# # c)
-# espinhosas = flores[:3]
+# c)
+espinhosas = flores[:3]
 
-# # d)
-# venenosas = flores[-1]
+# d)
+venenosas = flores[-1]
 
-# # e)
-# perigosas = [espinhosas] + [venenosas]
+# e)
+perigosas = [espinhosas] + [venenosas]
 
-# ==================================
+==================================
 
-# # 2.19
+# 2.19
 
-# def alvo(x, y):
-#     if (x <= 10 and y <= 10) and (x >= -10 and y >= -10):
-#         print('acertou!')
-#     else:
-#         print('parede!')
+def alvo(x, y):
+    if (x <= 10 and y <= 10) and (x >= -10 and y >= -10):
+        print('acertou!')
+    else:
+        print('parede!')
 
-# alvo(11, 0)
+alvo(11, 0)
 
-# ==================================
+==================================
 
-# # 2.20
+# 2.20
 
-# import math
+import math
 
-# def altura(comp, ang):
-#     rad = (math.pi * ang) / 180
-#     altura = math.sin(rad) * comp
-#     return altura
+def altura(comp, ang):
+    rad = (math.pi * ang) / 180
+    altura = math.sin(rad) * comp
+    return altura
 
-# print(altura(16, 75))
+print(altura(16, 75))
 
 # ==================================
 
